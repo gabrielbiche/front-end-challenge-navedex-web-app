2b5be0e9f25fcbded48a0c797780e468d4ed6160
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-09 13:18:41 -0300

    fix(components): corrected width

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,27 +2,34 @@ import styled from 'styled-components';
 
 import { Row, Image, Link } from 'components';
 import logo from 'assets/logo/logo.svg';
+import { useUser } from 'context/userContext';
 
 const NavBarComponent = () => {
+  const { logout } = useUser();
+
   return (
     <Row top={0}>
       <NavBar>
         <Link to='/home'>
           <Image src={logo} atl='logo' width={145.12} height={37} ml={32} />
         </Link>
-        <Link to={'/logout'} label='Sair' fontSize={14} fontWeight={600} mr={32} />
+        <Link 
+          to={'/login'} onClick={logout} label='Sair' fontSize={14} fontWeight={600} mr={32} />
       </NavBar>
     </Row>
   );
 };
 
 const NavBar = styled.div`
-  width: 100%;
+  width: 1280px;
   height: 85px;
+  left: 0px;
+  right: 0px;
+  margin: auto;
   display: flex;
+  z-index: 1;
   position: absolute;
   align-items: center;
-  z-index: 1;
   justify-content: space-between;
   background-color: ${({ theme }) => theme.colors.white};
 `;
