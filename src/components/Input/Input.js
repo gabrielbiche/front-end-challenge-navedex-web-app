4896b3ddb51020384bf9925acae2daaf5f9f0106
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-09 13:17:18 -0300

    feat(component): added reference to native component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,25 +1,25 @@
+/* eslint-disable */
+import React, { forwardRef } from 'react';
 import styled from 'styled-components';
 import { variant } from 'styled-system';
 
 import { Column, Text } from 'components';
 
-const InputComponent = ({ label, name, placeholder, error, type, ...props }) => {
-  return (
-    <Column mb={32}>
-      <Column height={62} position='relative'>
-        {label && (
-          <Text variant='small' fontWeight={600} marginBottom={4}>
-            {label}
-          </Text>
-        )}
-        <Input {...props} name={name} placeholder={placeholder} error={error} type={type} />
-        <Text position='absolute' top={64} color='red' variant='small'>
-          {error}
+const InputComponent = forwardRef(({ label, name, placeholder, error, type, ...props }, ref) => (
+  <Column mb={32}>
+    <Column height={62} position='relative'>
+      {label && (
+        <Text variant='small' fontWeight={600} marginBottom={4}>
+          {label}
         </Text>
-      </Column>
+      )}
+      <Input ref={ref} {...props} name={name} placeholder={placeholder} error={error} type={type} />
+      <Text position='absolute' top={64} color='red' variant='small'>
+        {error}
+      </Text>
     </Column>
-  );
-};
+  </Column>
+));
 
 const Input = styled('input')(
   {
