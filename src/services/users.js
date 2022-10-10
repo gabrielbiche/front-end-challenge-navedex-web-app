f94aabf59d4c8ef12f2719d5c01897e530b1e6a6
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-10 12:44:50 -0300

    feat(service): added route for logged in user query

--- src/services/users.js
+++ src/services/users.js
@@ -1,3 +1,5 @@
 import { client } from 'providers';
 
+export const getMe = () => client.get('/v1/me');
+
 export const login = (email, password) => client.post('/users/login', { email, password });
