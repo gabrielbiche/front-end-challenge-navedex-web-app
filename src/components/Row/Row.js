20e0a8ae1d0fd22c2c096b98405c862df9ea59c6
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-15 10:58:49 -0300

    feat(components): added border style, grid and color

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
-import { space, layout, flexbox, position, border } from 'styled-system';
+import { space, layout, flexbox, position, border, grid, color } from 'styled-system';
 
 const RowComponent = styled.div(
   {
-    display: 'flex'
+    display: 'flex',
   },
   flexbox,
   space,
   layout,
   position,
-  border
+  border,
+  grid,
+  color
 );
 
 export default RowComponent;
