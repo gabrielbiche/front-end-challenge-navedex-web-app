8db696b5b7e6ff4b3abca2deeedeb560ca75ffdd
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-14 23:14:26 -0300

    refactor(components):improved indentation

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -7,12 +7,12 @@ import Pen from './Pen';
 const IconComponent = ({ name }) => {
   const Icon = useMemo(() => {
     switch (name) {
-    case 'trash':
-      return Trash;
-    case 'arrow':
-      return Arrow;
-    case 'pen':
-      return Pen;
+      case 'trash':
+        return Trash;
+      case 'arrow':
+        return Arrow;
+      case 'pen':
+        return Pen;
     }
   }, [name]);
 
