61aabf4ae811a764603fbf185096b312a5efe454
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-15 11:06:40 -0300

    refactor(routes): improved route name

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,15 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { Home, AddNaver, EditNaver } from 'screens';
+import { NaversList, NaverForm } from 'screens';
 
 const AuthenticatedApp = () => {
   return (
     <Routes>
-      <Route path='*' element={<Navigate to='/home' />} />
+      <Route path='/navers' element={<NaversList />} />
+      <Route exact path='/navers/create' element={<NaverForm />} />
+      <Route exact path='/navers/:id' element={<NaverForm />} />
 
-      <Route path='/home' element={<Home />} />
-      <Route path='/add-naver' element={<AddNaver />} />
-      <Route path='/edit-naver' element={<EditNaver />} />
+      <Route path='*' element={<Navigate to='/navers' />} />
     </Routes>
   );
 };
