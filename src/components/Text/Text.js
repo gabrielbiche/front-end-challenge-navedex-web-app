b0251f865ff65ac96a35b11898ddb9f75fef007c
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-20 14:10:32 -0300

    feat(components): changed text variation

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -2,9 +2,7 @@ import styled from 'styled-components';
 import { space, layout, typography, color, position, variant } from 'styled-system';
 
 const BIG = 'big';
-const MEDIUM = 'medium';
 const REGULAR = 'regular';
-const SMALL = 'small';
 
 const TextComponent = styled.p(
   space,
@@ -16,19 +14,11 @@ const TextComponent = styled.p(
     variants: {
       [BIG]: {
         fontSize: 24,
-        lineHeight: '29px'
-      },
-      [MEDIUM]: {
-        fontSize: 20,
-        lineHeight: '24px'
+        lineHeight: '36px'
       },
       [REGULAR]: {
         fontSize: 16,
-        lineHeight: '25px'
-      },
-      [SMALL]: {
-        fontSize: 14,
-        lineHeight: '18px'
+        lineHeight: '24px'
       }
     }
   })
