41476e93aaccfb0c733466dad9f19eabe02eda79
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-20 14:20:41 -0300

    feat(formatters): added period formation methods in years and months

--- src/helpers/formatters.js
+++ src/helpers/formatters.js
@@ -1,4 +1,28 @@
-import { format, addMinutes } from 'date-fns';
+import { format, addMinutes, intervalToDuration } from 'date-fns';
 
 export const formatDateFromApi = date =>
   format(addMinutes(new Date(date), new Date().getTimezoneOffset()), 'dd/MM/yyyy');
+
+export const formatPeriodInYears = dateString => {
+  const { years } = intervalToDuration({ start: new Date(dateString), end: new Date() });
+  const yearsFormatted = `${years} anos`;
+  return yearsFormatted;
+};
+
+export const formatPeriodInYearsAndMonths = dateString => {
+  const { years, months } = intervalToDuration({ start: new Date(dateString), end: new Date() });
+  const yearsFormatted = years > 1 ? `${years} anos` : `${years} ano`;
+  const monthsFormatted = months > 1 ? `${months} meses` : `${months} mês`;
+
+  let periodFormatted = '';
+
+  if (years && months) {
+    periodFormatted = `${yearsFormatted} e ${monthsFormatted}`;
+  } else if (years) {
+    periodFormatted = `${yearsFormatted}`;
+  } else {
+    periodFormatted = `${monthsFormatted}`;
+  }
+
+  return periodFormatted;
+};
