ad3fe61fccafcb50e6b5292cd00c42caed9503bc
gabrielsbiche <gabrielsbiche@gmail.com>
2022-10-20 10:49:11 -0300

    feat(components): added properties for button manipulation

--- src/components/Icon/Trash.js
+++ src/components/Icon/Trash.js
@@ -1,9 +1,12 @@
 import Svg from './Svg';
 
-const Trash = () => {
+const Trash = ({ ...props }) => {
   return (
-    <Svg width={14} height={18}>
-      <path d='M1 18H13V4H1V18ZM14 1H10.5L9.5 0H4.5L3.5 1H0V3H14V1Z' />
+    <Svg width={14} height={18} {...props}>
+      <path 
+        d='M1 18H13V4H1V18ZM14 1H10.5L9.5 0H4.5L3.5 1H0V3H14V1Z' 
+        fill='#212121' 
+      />
     </Svg>
   );
 };
